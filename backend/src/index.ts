399e935ca25276cec665b394300c130e1946b04e
Sachin Pal <sachinpal@Sachins-MacBook-Pro.local>
2025-06-17 11:43:54 +0530

    Backend - created healthCheck

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,7 +1,27 @@
+import "dotenv/config";
 import express from "express";
+import cors from "cors";
+import connectToDatabase from "./config/db";
+import { APP_ORIGIN, NODE_ENV, PORT } from "./constants/env";
+import cookieParser from "cookie-parser";
 
 const app = express();
 
-app.listen(4004, () => {
-  console.log(`server is running in port 4004 in development enviourment`);
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(
+  cors({
+    origin: APP_ORIGIN,
+    credentials: true,
+  })
+);
+app.use(cookieParser());
+
+app.use("/", (req, res) => {
+  res.send({ message: "healthy" });
+});
+
+app.listen(PORT, async () => {
+  console.log(`server is running in port ${PORT} in ${NODE_ENV} enviourment`);
+  await connectToDatabase();
 });
