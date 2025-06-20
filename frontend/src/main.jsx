1f7a2b1fa295bfb5323e7462c81889cc4d84e1f4
Sachin Pal <sachinpal@Sachins-MacBook-Pro.local>
2025-06-20 22:08:58 +0530

    Frontend - config chakra-ui

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,17 +2,20 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
+import { Provider } from "./components/ui/provider.jsx";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { QueryClientProvider } from "@tanstack/react-query";
-import queryClient from "./Config/queryClient.js";
+import queryClient from "./config/queryClient.js";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <BrowserRouter>
-        <App />
-        <ReactQueryDevtools position="bottom-right" initialIsOpen={false} />
-      </BrowserRouter>
-    </QueryClientProvider>
+    <Provider>
+      <QueryClientProvider client={queryClient}>
+        <BrowserRouter>
+          <App />
+          <ReactQueryDevtools position="bottom-right" initialIsOpen={false} />
+        </BrowserRouter>
+      </QueryClientProvider>
+    </Provider>
   </StrictMode>
 );
